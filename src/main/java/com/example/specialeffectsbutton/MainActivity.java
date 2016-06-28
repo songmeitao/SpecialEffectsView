31ae600ff4496160eead9adec681c31f26c38100
smt <song_meitao@163.com>
2016-06-28 14:34:40 +0800

    ==

---
+++ src/main/java/com/example/specialeffectsbutton/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.specialeffectsbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wingsofts.simplelinechart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
